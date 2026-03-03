e4eb1b7885e7c60bece302d99bb9cbe603309723
ng-caixian <ngcaixian@gmail.com>
2026-03-04 03:22:39 +0800

    updated home page

--- HabitatHero/src/frontend/src/components/FeatureCard.js
+++ HabitatHero/src/frontend/src/components/FeatureCard.js
@@ -1,6 +1,10 @@
-function FeatureCard({ title, description }) {
+function FeatureCard({ icon, title, description }) {
   return (
-    <div>
+    <div className="feature-card">
+      <div className="feature-icon">
+        {icon}
+      </div>
+
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
