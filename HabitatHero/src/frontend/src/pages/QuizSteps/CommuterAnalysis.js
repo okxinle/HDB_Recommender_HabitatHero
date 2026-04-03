cedf8c89df209dc23f6a9c9ad9f6cf2cb93c56dd
ng-caixian <ngcaixian@gmail.com>
2026-04-03 15:36:48 +0800

    clear commuter destination inputs when commuter analysis is disabled

--- HabitatHero/src/frontend/src/pages/QuizSteps/CommuterAnalysis.js
+++ HabitatHero/src/frontend/src/pages/QuizSteps/CommuterAnalysis.js
@@ -11,6 +11,7 @@ function CommuterAnalysis({ data, update, showErrors }) {
       commuterProfile: {
         ...profile,
         enabled: !profile.enabled,
+        destinations: profile.enabled ? ["", ""] : profile.destinations,
       },
     });
   };
