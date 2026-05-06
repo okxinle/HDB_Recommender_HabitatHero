ee8e6ea9653b1e27a96e37872e12b8bcc46e295d
Chong Xin Le <99386726+okxinle@users.noreply.github.com>
2026-05-06 10:19:38 +0800

    Delete Lab 4 Submissions directory

--- Lab 4 Submissions/HabitatHero/src/main/java/habitathero/repository/GlobalConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package habitathero.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import habitathero.entity.GlobalWeightConfig;
-
-public interface GlobalConfigRepository extends JpaRepository<GlobalWeightConfig, String> {}
